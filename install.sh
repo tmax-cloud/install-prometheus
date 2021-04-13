24457e21c4c9017efaba609de3ac7238f17e3337
chosangwon93 <sangwon_cho@tmax.co.kr>
2021-04-13 13:27:35 +0900

    [mod] install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
- sudo chmod +x version.conf
- source version.conf
+#!/bin/bash
+  
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MANIFEST_HOME=$SCRIPTDIR/yaml/manifests
+SETUP_HOME=$SCRIPTDIR/yaml/setup
 
-cd yaml
-cd manifests
+source $SCRIPTDIR/version.conf
 
-sed -i 's/{ALERTMANAGER_VERSION}/'${ALERTMANAGER_VERSION}'/g' alertmanager-alertmanager.yaml
-sed -i 's/{KUBE_RBAC_PROXY_VERSION}/'${KUBE_RBAC_PROXY_VERSION}'/g' kube-state-metrics-deployment.yaml
-sed -i 's/{KUBE_STATE_METRICS_VERSION}/'${KUBE_STATE_METRICS_VERSION}'/g' kube-state-metrics-deployment.yaml
-sed -i 's/{NODE_EXPORTER_VERSION}/'${NODE_EXPORTER_VERSION}'/g' node-exporter-daemonset.yaml
-sed -i 's/{KUBE_RBAC_PROXY_VERSION}/'${KUBE_RBAC_PROXY_VERSION}'/g' node-exporter-daemonset.yaml
-sed -i 's/{PROMETHEUS_ADAPTER_VERSION}/'${PROMETHEUS_ADAPTER_VERSION}'/g' prometheus-adapter-deployment.yaml
-sed -i 's/{PROMETHEUS_VERSION}/'${PROMETHEUS_VERSION}'/g' prometheus-prometheus.yaml
+
+sed -i 's/{ALERTMANAGER_VERSION}/'${ALERTMANAGER_VERSION}'/g' $MANIFEST_HOME/alertmanager-alertmanager.yaml
+sed -i 's/{KUBE_RBAC_PROXY_VERSION}/'${KUBE_RBAC_PROXY_VERSION}'/g' $MANIFEST_HOME/kube-state-metrics-deployment.yaml
+sed -i 's/{KUBE_STATE_METRICS_VERSION}/'${KUBE_STATE_METRICS_VERSION}'/g' $MANIFEST_HOME/kube-state-metrics-deployment.yaml
+sed -i 's/{NODE_EXPORTER_VERSION}/'${NODE_EXPORTER_VERSION}'/g' $MANIFEST_HOME/node-exporter-daemonset.yaml
+sed -i 's/{KUBE_RBAC_PROXY_VERSION}/'${KUBE_RBAC_PROXY_VERSION}'/g' $MANIFEST_HOME/node-exporter-daemonset.yaml
+sed -i 's/{PROMETHEUS_ADAPTER_VERSION}/'${PROMETHEUS_ADAPTER_VERSION}'/g' $MANIFEST_HOME/prometheus-adapter-deployment.yaml
+sed -i 's/{PROMETHEUS_VERSION}/'${PROMETHEUS_VERSION}'/g' $MANIFEST_HOME/prometheus-prometheus.yaml
 
 if [ $REGISTRY != "{REGISTRY}" ]; then
-	sed -i "s/quay.io\/prometheus\/alertmanager/${REGISTRY}\/prometheus\/alertmanager/g" alertmanager-alertmanager.yaml
-	sed -i "s/quay.io\/coreos\/kube-rbac-proxy/${REGISTRY}\/coreos\/kube-rbac-proxy/g" kube-state-metrics-deployment.yaml
-	sed -i "s/quay.io\/coreos\/kube-state-metrics/${REGISTRY}\/coreos\/kube-state-metrics/g" kube-state-metrics-deployment.yaml
-	sed -i "s/quay.io\/prometheus\/node-exporter/${REGISTRY}\/prometheus\/node-exporter/g" node-exporter-daemonset.yaml
-	sed -i "s/quay.io\/coreos\/kube-rbac-proxy/${REGISTRY}\/coreos\/kube-rbac-proxy/g" node-exporter-daemonset.yaml
-	sed -i "s/quay.io\/coreos\/k8s-prometheus-adapter-amd64/${REGISTRY}\/coreos\/k8s-prometheus-adapter-amd64/g" prometheus-adapter-deployment.yaml
-	sed -i "s/quay.io\/prometheus\/prometheus/${REGISTRY}\/prometheus\/prometheus/g" prometheus-prometheus.yaml
+	sed -i "s/quay.io\/prometheus\/alertmanager/${REGISTRY}\/prometheus\/alertmanager/g" $MANIFEST_HOME/alertmanager-alertmanager.yaml
+	sed -i "s/quay.io\/coreos\/kube-rbac-proxy/${REGISTRY}\/coreos\/kube-rbac-proxy/g" $MANIFEST_HOME/kube-state-metrics-deployment.yaml
+	sed -i "s/quay.io\/coreos\/kube-state-metrics/${REGISTRY}\/coreos\/kube-state-metrics/g" $MANIFEST_HOME/kube-state-metrics-deployment.yaml
+	sed -i "s/quay.io\/prometheus\/node-exporter/${REGISTRY}\/prometheus\/node-exporter/g" $MANIFEST_HOME/node-exporter-daemonset.yaml
+	sed -i "s/quay.io\/coreos\/kube-rbac-proxy/${REGISTRY}\/coreos\/kube-rbac-proxy/g" $MANIFEST_HOME/node-exporter-daemonset.yaml
+	sed -i "s/quay.io\/coreos\/k8s-prometheus-adapter-amd64/${REGISTRY}\/coreos\/k8s-prometheus-adapter-amd64/g" $MANIFEST_HOME/prometheus-adapter-deployment.yaml
+	sed -i "s/quay.io\/prometheus\/prometheus/${REGISTRY}\/prometheus\/prometheus/g" $MANIFEST_HOME/prometheus-prometheus.yaml
 fi
 
-cd ..
-
-cd setup
 if [ $REGISTRY != "{REGISTRY}" ]; then
 	sed -i "s/quay.io\/coreos\/configmap-reload/${REGISTRY}\/coreos\/configmap-reload/g" prometheus-operator-deployment.yaml
 	sed -i "s/quay.io\/coreos\/prometheus-config-reloader/${REGISTRY}\/coreos\/prometheus-config-reloader/g" prometheus-operator-deployment.yaml
@@ -32,25 +32,23 @@ if [ $REGISTRY != "{REGISTRY}" ]; then
 		 
 fi
 
-
-sed -i 's/{PROMETHEUS_OPERATOR_VERSION}/'${PROMETHEUS_OPERATOR_VERSION}'/g' prometheus-operator-deployment.yaml
-sed -i 's/{CONFIGMAP_RELOADER_VERSION}/'${CONFIGMAP_RELOADER_VERSION}'/g' prometheus-operator-deployment.yaml
-sed -i 's/{CONFIGMAP_RELOAD_VERSION}/'${CONFIGMAP_RELOAD_VERSION}'/g' prometheus-operator-deployment.yaml
-
-		 
-cd ..
+sed -i 's/{PROMETHEUS_OPERATOR_VERSION}/'${PROMETHEUS_OPERATOR_VERSION}'/g' $SETUP_HOME/prometheus-operator-deployment.yaml
+sed -i 's/{CONFIGMAP_RELOADER_VERSION}/'${CONFIGMAP_RELOADER_VERSION}'/g' $SETUP_HOME/prometheus-operator-deployment.yaml
+sed -i 's/{CONFIGMAP_RELOAD_VERSION}/'${CONFIGMAP_RELOAD_VERSION}'/g' $SETUP_HOME/prometheus-operator-deployment.yaml
 
 if ! command -v yq 2>/dev/null ; then
   wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -O /usr/bin/yq &&\
   chmod +x /usr/bin/yq
 fi
 
-kubectl get pod `kubectl get pod -n kube-system -o jsonpath='{.items[?(@.metadata.labels.component == "kube-scheduler")].metadata.name}'` -n kube-system -o yaml > kube-scheduler.yaml
-yq e '.metadata.labels.k8s-app = "kube-scheduler"' -i ./kube-scheduler.yaml
-kubectl apply -f kube-scheduler.yaml
-kubectl get pod `kubectl get pod -n kube-system -o jsonpath='{.items[?(@.metadata.labels.component == "kube-controller-manager")].metadata.name}'` -n kube-system -o yaml > kube-controller-manager.yaml
-yq e '.metadata.labels.k8s-app = "kube-controller-manager"' -i ./kube-controller-manager.yaml
-kubectl apply -f kube-controller-manager.yaml
 
-kubectl create -f setup/
-kubectl create -f manifests/
+kubectl get pod `kubectl get pod -n kube-system -o jsonpath='{.items[?(@.metadata.labels.component == "kube-scheduler")].metadata.name}'` -n kube-system -o yaml > $SCRIPT_DIR/kube-scheduler.yaml
+yq e '.metadata.labels.k8s-app = "kube-scheduler"' -i $SCRIPT_DIR/kube-scheduler.yaml
+sudo mv $SCRIPT_DIR/kube-scheduler.yaml /etc/manifest/kubernetes/
+
+kubectl get pod `kubectl get pod -n kube-system -o jsonpath='{.items[?(@.metadata.labels.component == "kube-controller-manager")].metadata.name}'` -n kube-system -o yaml > $SCRIPT_DIR/kube-controller-manager.yaml
+yq e '.metadata.labels.k8s-app = "kube-controller-manager"' -i $SCRIPT_DIR/kube-controller-manager.yaml
+sudo mv $SCRIPT_DIR/kube-controller-manager.yaml /etc/manifest/kubernetes/
+
+kubectl create -f $SETUP_HOME
+kubectl create -f $MANIFEST_HOME
