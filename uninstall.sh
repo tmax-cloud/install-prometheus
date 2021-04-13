287773b7d6e80042c111e9845762ded4ae63e4ce
chosangwon93 <sangwon_cho@tmax.co.kr>
2021-04-13 13:42:43 +0900

    [mod] uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,9 @@
-cd yaml/
+#!/bin/bash
+  
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MANIFEST_HOME=$SCRIPTDIR/yaml/manifests
+SETUP_HOME=$SCRIPTDIR/yaml/setup
 
-kubectl delete -f manifests/
-sleep 30s
-kubectl delete -f setup/
+kubectl delete -f $MANIFEST_HOME
+#sleep 30s
+kubectl delete -f $SETUP_HOME
