c5209f8a9a4f46fec42849626ece219d68e431cb
bongwoobak <66110096+bongwoobak@users.noreply.github.com>
2021-04-19 12:02:13 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,13 +42,13 @@ if ! command -v yq 2>/dev/null ; then
 fi
 
 
-kubectl get pod `kubectl get pod -n kube-system -o jsonpath='{.items[?(@.metadata.labels.component == "kube-scheduler")].metadata.name}'` -n kube-system -o yaml > $SCRIPTDIR/kube-scheduler.yaml
-sudo yq e '.metadata.labels.k8s-app = "kube-scheduler"' -i $SCRIPTDIR/kube-scheduler.yaml
-sudo mv $SCRIPTDIR/kube-scheduler.yaml /etc/kubernetes/manifests/
+#kubectl get pod `kubectl get pod -n kube-system -o jsonpath='{.items[?(@.metadata.labels.component == "kube-scheduler")].metadata.name}'` -n kube-system -o yaml > $SCRIPTDIR/kube-scheduler.yaml
+#sudo yq e '.metadata.labels.k8s-app = "kube-scheduler"' -i $SCRIPTDIR/kube-scheduler.yaml
+#sudo mv $SCRIPTDIR/kube-scheduler.yaml /etc/kubernetes/manifests/
 
-kubectl get pod `kubectl get pod -n kube-system -o jsonpath='{.items[?(@.metadata.labels.component == "kube-controller-manager")].metadata.name}'` -n kube-system -o yaml > $SCRIPTDIR/kube-controller-manager.yaml
-sudo yq e '.metadata.labels.k8s-app = "kube-controller-manager"' -i $SCRIPTDIR/kube-controller-manager.yaml
-sudo mv $SCRIPTDIR/kube-controller-manager.yaml /etc/kubernetes/manifests/
+#kubectl get pod `kubectl get pod -n kube-system -o jsonpath='{.items[?(@.metadata.labels.component == "kube-controller-manager")].metadata.name}'` -n kube-system -o yaml > $SCRIPTDIR/kube-controller-manager.yaml
+#sudo yq e '.metadata.labels.k8s-app = "kube-controller-manager"' -i $SCRIPTDIR/kube-controller-manager.yaml
+#sudo mv $SCRIPTDIR/kube-controller-manager.yaml /etc/kubernetes/manifests/
 
 kubectl create -f $SETUP_HOME
 kubectl create -f $MANIFEST_HOME
