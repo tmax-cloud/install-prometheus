8cec3d92c4b8eaa80310f83176962c2ff85e14ee
bongwoobak <66110096+bongwoobak@users.noreply.github.com>
2021-06-14 16:58:23 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,6 +7,15 @@ MANIFEST_HOME=$SCRIPTDIR/yaml/manifests
 SETUP_HOME=$SCRIPTDIR/yaml/setup
 source $SCRIPTDIR/version.conf
 
+if ! command -v yq 2>/dev/null ; then
+  sudo wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -O /usr/bin/yq &&\
+  chmod +x /usr/bin/yq
+fi
+
+if ! command -v sshpass 2>/dev/null ; then
+  sudo yum install https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/sshpass-1.06-9.el8.x86_64.rpm
+  # yum install sshpass
+fi
 
 sudo cp /etc/kubernetes/manifests/kube-scheduler.yaml .
 sudo yq eval 'del(.metadata.labels.k8s-app)' -i kube-scheduler.yaml
@@ -31,7 +40,7 @@ do
     continue
   fi
   i=$((i+1))
-  sudo sshpass -p "${MASTER_NODE_ROOT_PASSWORD[i]}" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER[i]}@"$master" yum install yq
+  sudo sshpass -p "${MASTER_NODE_ROOT_PASSWORD[i]}" scp /usr/bin/yq ${MASTER_NODE_ROOT_USER[i]}@"$master":/usr/bin/yq
   sudo sshpass -p "${MASTER_NODE_ROOT_PASSWORD[i]}" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER[i]}@"$master" sudo cp /etc/kubernetes/manifests/etcd.yaml .
   sudo sshpass -p "${MASTER_NODE_ROOT_PASSWORD[i]}" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER[i]}@"$master" 'sudo yq e '"'"'del(.metadata.labels.k8s-app)'"'"' -i etcd.yaml'
   sudo sshpass -p "${MASTER_NODE_ROOT_PASSWORD[i]}" ssh -o StrictHostKeyChecking=no ${MASTER_NODE_ROOT_USER[i]}@"$master" 'sudo yq eval '"'"'(.spec.containers[0].command[] | select(. == "--listen-metrics-urls*")) = "--listen-metrics-urls=http://127.0.0.1:2381"'"'"' -i etcd.yaml'
